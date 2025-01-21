45160233d68e0e35a9bc1f3f5a881a8dd8aaeff5
Zahid hasan <zmzahidhasan181@gmail.com>
2025-01-21 14:24:49 +0600

    fixed tailwind

--- src/assets/Component/Navbar.jsx
+++ src/assets/Component/Navbar.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
+import { MdDirectionsBike } from "react-icons/md";
+import { BsBagHeart } from "react-icons/bs";
+import { HiOutlineShoppingCart } from "react-icons/hi2";
+import { FaRegCircleUser } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-   <>
-   <div>
-    
-   </div>
-   </>
-  )
-}
+    <>
+      <div className="bg-[#a53535] pt-4 pr-0 pb-6 pl-0 ">
+        <div>Kawasaki</div>
+        <div>
+          <div>
+            <div>
+              <ul>
+                <div>
+                  <FaRegCircleUser />
+                </div>
+                <li>My Kawasaki</li>
+              </ul>
+              <ul>
+                <div>
+                  <HiOutlineShoppingCart />
+                </div>
+                <li>Cart</li>
+              </ul>
+              <ul>
+                <div>
+                  <BsBagHeart />
+                </div>
+                <li>Wish List</li>
+              </ul>
+              <ul>
+                <div>
+                  <MdDirectionsBike />
+                </div>
+                <li>Test Ride</li>
+              </ul>
+              <ul>
+                <div>
+                  <IoLocationOutline />
+                </div>
+                <li>Find Dealer</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div><input type='text' placeholder="Search"/></div>
+            <div><IoIosSearch/></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        OswaldSerif: ["Oswald","serif"],
+        FiraSerif: ["Fira Sans","serif"], 
+        BlinkerSerif:["Blinker", "serif"]
+          },
+    },
   },
   plugins: [],
 }
